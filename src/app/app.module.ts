ab5271a23646447b5da452d7124536eddeade0a9
pritamnb <preetambhalerao00@gmail.com>
2019-02-18 00:33:04 +0530

    latest update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,23 +22,16 @@ import { Shared1Module } from '../app/shared1/shared1.module';
 import { NgxEditorModule } from 'ngx-editor';
 import { MaterialModule } from '../material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FroalaEditorModule, FroalaViewModule } from 'angular-froala-wysiwyg';
 // import { PaginationComponent } from './shared/components/pagination/pagination.component';
 import { DlDateTimePickerDateModule } from 'angular-bootstrap-datetimepicker';
 import { FlatpickrModule } from 'angularx-flatpickr';
 import { AngularMarkdownEditorModule } from 'angular-markdown-editor';
 import { SimplemdeModule, SimplemdeConfig } from 'ngx-simplemde';
-// import { HttpClient } from '@angular/common/http';
-// import { SimplemdeModule, SIMPLEMDE_CONFIG } from 'ng2-simplemde';
-// import { MarkdownCustomComponent } from './markdown-editor-custom-format.component';
-import { RichTextEditorAllModule } from '@syncfusion/ej2-angular-richtexteditor';
 
-// rich text editor custom
-// import { RichTextEditorAllModule } from '@syncfusion/ej2-angular-richtexteditor';
-// import { CheckBoxModule } from '@syncfusion/ej2-angular-buttons';
-// import { NumericTextBoxModule } from '@syncfusion/ej2-angular-inputs';
-// import { DialogModule } from '@syncfusion/ej2-angular-popups';
+import { RichTextEditorAllModule } from '@syncfusion/ej2-angular-richtexteditor';
+import { DatePickerModule } from '@syncfusion/ej2-angular-calendars';
 @NgModule({
   declarations: [
     AppComponent,
@@ -75,6 +68,8 @@ import { RichTextEditorAllModule } from '@syncfusion/ej2-angular-richtexteditor'
     FlatpickrModule.forRoot(),
     AngularMarkdownEditorModule,
     RichTextEditorAllModule,
+    ReactiveFormsModule,
+    DatePickerModule
     // CheckBoxModule,
     // NumericTextBoxModule,
     // DialogModule
